367be5ba937ab5d91b6c4e285b2de91fd8c4fee3
Kritika Kanwar <kritikakanwar27@gmail.com>
2024-02-06 09:50:25 +0530

    Add files via upload
    Beginner basic python programming

---
+++ Basic_python.py
@@ -0,0 +1,10 @@
+
+#A Basic example to start your programming in python 
+ #band name generator
+
+name=input("what's your name ?")
+print(name)
+petName=input("what's your pet name ?")
+print(petName)
+band_Name=name+" "+petName
+print("Your band name could be :",band_Name)
