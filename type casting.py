33db5d74b4bea97b6710d06dc6ce0447f340a47f
Kritika Kanwar <kritikakanwar27@gmail.com>
2024-02-06 17:02:28 +0530

    Type Casting and Precedance of operators

--- type casting.py
+++ type casting.py
@@ -1,10 +1,28 @@
 #Type conversion/casting- refers to the process of changimg the value
 #of one datatype to another datatype
-#commonly used tyype casting function are str,int ,list,float
+#commonly used type casting function are str,int ,list,float
 
 number=str(46)
-digit1=int(text1[0])
-digit2=int(text1[1])
+digit1=int(number[0])
+digit2=int(number[1])
 sum=digit1+digit2
 print(sum)
 print(str(digit1)+ "+" +str(digit2)+ "=" +str(sum))
+
+#order of priority
+#PEMDAS
+#Paranthesis
+#Exponents
+#Multiplication
+#Division
+#Addition
+#Subtraction
+
+print(3*3+3/3-3)
+#o/p=7.0
+#Precedence of operators is from left to right .Here multplication comes first before division. so mulplication is performed first.
+
+print(6/3-3*3+3)
+o/p=-4
+#likewise, In this subtration got higher precedance than addition.
+
