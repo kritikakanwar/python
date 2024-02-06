0cc54fd70fa32b13b5ec2230c0b0981d458c2e49
Kritika Kanwar <kritikakanwar27@gmail.com>
2024-02-06 16:42:36 +0530

    Type casting in python

---
+++ type casting.py
@@ -0,0 +1,10 @@
+#Type conversion/casting- refers to the process of changimg the value
+#of one datatype to another datatype
+#commonly used tyype casting function are str,int ,list,float
+
+number=str(46)
+digit1=int(text1[0])
+digit2=int(text1[1])
+sum=digit1+digit2
+print(sum)
+print(str(digit1)+ "+" +str(digit2)+ "=" +str(sum))
