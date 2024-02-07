9d4f0fa0d4c00837fc5b9d38adde3e6c8f181865
Kritika Kanwar <kritikakanwar27@gmail.com>
2024-02-07 11:11:57 +0530

    Third Commit .Updated few lines of code

--- tip_cal.py
+++ tip_cal.py
@@ -1,31 +1,35 @@
 #tip calculator
-
+#Using f string 
+#1
 print("Welcome to tip calculator\n")
-total_bill=float(input("what was the total bill ?"))
+bill=float(input("what was the total bill ?"))
 total_persons=int(input("Split bill into how many persons ?"))
 tip=int(input("what percentile of Tip persons would like to give on Total amount. 10,12 or 15"))
 if tip in (10,12,15):
     print("choosen correct option")
-    a=(total_bill*(tip/100))/total_persons
+    a=(bill*(tip/100))/total_persons
     print(a)
-    Per_person_amount=(total_bill/total_persons)+a
+    per_person_amount=(bill/total_persons)+a
+    print(f"Each person should pay :{per_person_amount}")
 else:
     print("Select Standard Tip percentage 10,12 or 15")
     
-print("Each person should pay :",Per_person_amount)
 
+
+#2
 print("Welcome to tip calculator\n")
-total_bill=float(input("what was the total bill ?"))
+bill=float(input("what was the total bill ?"))
 total_persons=int(input("Split bill into how many persons ?"))
 tip=int(input("what percentile of Tip persons would like to give on Total amount. 10,12 or 15"))
 if tip in (10,12,15):
     print("choosen correct option")
-    tip_calculation=total_bill*(tip/100)
-    total_amt=total_bill+tip_calculation
-    per_Person_Amt=total_amt/total_persons
-    
+    tip_calculation=float(bill*(tip/100))
+    total_bill=bill+tip_calculation
+    per_Person_Amt=total_bill/total_persons
+    print(f"Each person should pay :{per_Person_Amt}")
 else:
     print("Select Standard Tip percentage 10,12 or 15")
-print("Each person should pay :",per_Person_Amt)
+
+
 
 
